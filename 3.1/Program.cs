426e8cd7df8302f2539a623bb5fa4844722f0863
Falak <falak2hirani@gmail.com>
2022-02-02 13:48:28 +0530

    first commit

---
+++ 3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num, deno;
+
+            Console.WriteLine("Enter Numerator : ");
+            //   num = int.Parse(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Denominator : ");
+            deno = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = num / deno;
+            int remainder = num % deno;
+
+            Console.WriteLine($"Integer division result : {quotient} with a remainder : {remainder}");
+
+            double frac_quotient = Math.Round((double)num / deno);
+
+            Console.WriteLine($"Fractional Quotient : {frac_quotient}");
+            Console.WriteLine($"Mixed Fraction : {quotient} {remainder}/{deno}");
+        }
+    }
+}
---
+++ 3.2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Program
+{
+
+    class ExString
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("<<-------- FINDING THE LENGTH OF A STRING ------------>>");
+            string s;
+            Console.WriteLine($"Enter a String : ");
+            s = Console.ReadLine();
+
+            Console.WriteLine($"The length of the String is : {s.Length}\n");
+
+
+
+
+            Console.WriteLine("<<---------- Checking The Type of The String------------>>");
+            Console.WriteLine($"Enter a String : ");
+            s = Console.ReadLine();
+
+            int len = s.Length;
+
+            if ((char)s[len - 1] == '.')
+            {
+                Console.WriteLine($"String \"{s}\" is Declarative Sentence");
+            }
+            else if ((char)s[len - 1] == '!')
+            {
+                Console.WriteLine($"String \"{s}\" is Exclamation Sentence");
+            }
+            else if ((char)s[len - 1] == '?')
+            {
+                Console.WriteLine($"String \"{s}\" is Interrogatory Sentence");
+            }
+            else
+            {
+                Console.WriteLine($"String \"{s}\" is not a  Sentence");
+            }
+
+            Console.WriteLine("<<--------- Checking The Type of The String------------->>");
+            Console.WriteLine("Enter your Name : ");
+            s = Console.ReadLine();
+
+            int index = s.IndexOf(" ");
+
+
+            if (index != -1)
+            {
+                // Console.WriteLine($"{index}");
+                // Console.WriteLine($"{len}");
+
+                string firstName = s.Substring(0, index);
+                string lastName = s.Substring(index + 1, len - index - 1);
+                Console.WriteLine($"{lastName} , {firstName}");
+            }
+            else
+                Console.WriteLine($"{s}");
+
+        }
+    }
+}
